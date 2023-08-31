9bf41c4179b8ba6e6d0a7550415e42b9fc9c7895
rupamShaw1998 <rupamshw77777@gmail.com>
2023-08-31 13:47:06 +0530

    optimized

--- src/components/TagView.jsx
+++ src/components/TagView.jsx
@@ -30,7 +30,6 @@ const TagView = ({ tag, sendData }) => {
           <input
             type="text"
             defaultValue={tag.name}
-            // onChange={(e) => handleTagChange("name", e.target.value)}
             onKeyUp={nameChange}
           />
         ) : (
@@ -68,55 +67,5 @@ const TagView = ({ tag, sendData }) => {
   );
 };
 
-// function ChildObject({ index, tag, sendData }) {
-//   const handleTagChange = (property, value) => {
-//     tag = { ...tag, [property]: value };
-//     sendData(tag);
-//   };
-
-//   const addChild = () => {
-//     const newChild = {name: "NewChild", data: "Data"};
-//     delete tag.data;
-//     const updatedChildren = tag.children? [...tag.children, newChild]: [newChild];
-//     const newTag = {...tag, children: updatedChildren};
-//     sendData(newTag);
-//   };
-
-//   return (
-//     <div style={{ border: "1px solid red", margin: "50px", textAlign: "left" }}>
-//       <div style={{ border: "1px solid blue", backgroundColor: "yellow" }}>
-//         <span>
-//           {tag.name}
-//           <button onClick={addChild}>Add Child</button>
-//         </span>
-//       </div>
-
-//       {!(tag.data === undefined) && (
-//         <div>
-//           <span>Data </span>
-//           <input
-//             type="text"
-//             value={tag.data}
-//             onChange={(e) => handleTagChange("data", e.target.value)}
-//             placeholder="data"
-//           />
-//         </div>
-//       )}
-
-//       {tag.children &&
-//         tag.children.map((child, index) => (
-//           <ChildObject
-//             key={index}
-//             index={index}
-//             tag={child}
-//             sendData={(updatedTag) => {
-//               tag.children[index] = updatedTag;
-//               sendData(tag);
-//             }}
-//           />
-//         ))}
-//     </div>
-//   );
-// }
 
 export default TagView;
